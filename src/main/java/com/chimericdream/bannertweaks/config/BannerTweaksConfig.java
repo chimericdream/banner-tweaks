c4cd52559f4c39a34f906527b241fe8dddf5c957
Bill Parrott <bill@chimericdream.com>
2024-10-14 22:12:20 -0500

    feat: bump max possible layers to 32

--- src/main/java/com/chimericdream/bannertweaks/config/BannerTweaksConfig.java
+++ src/main/java/com/chimericdream/bannertweaks/config/BannerTweaksConfig.java
@@ -13,7 +13,7 @@ public class BannerTweaksConfig implements ConfigData {
     public int maxBannerLayers = Defaults.MAX_BANNER_LAYERS;
 
     public void validatePostLoad() {
-        if (this.maxBannerLayers < 0 || this.maxBannerLayers > 16) {
+        if (this.maxBannerLayers < 0 || this.maxBannerLayers > 32) {
             BannerTweaksMod.LOGGER.info("[config] Invalid value found for 'maxBannerLayers'! Resetting to default.");
             this.maxBannerLayers = Defaults.MAX_BANNER_LAYERS;
         }
